057a118d18ca0c048e18611034235f375f4d0b65
SayedNawid <sayednawidmohammady@gmail.com>
2023-05-02 20:35:39 +0430

    add two expanded function

--- lib/main.dart
+++ lib/main.dart
@@ -14,3 +14,42 @@ void main() {
     ),
   ));
 }
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+               
+              },
+              child: Image.asset('images/dice$leftCountBtn.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                 
+                });
+              },
+              child: Image.asset("images/dice$rightCountBtn.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
